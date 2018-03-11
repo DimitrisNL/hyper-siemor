d490428a602b974dea0d6c264469e3781ca71866
Dimitrios Lytras <dnlytras@gmail.com>
2018-03-11 02:04:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Siemor
+
+A Hyper terminal theme for closers
+
+### Palette
+
+![palette](assets/palette.png)
---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports.decorateConfig = config => {
+  return Object.assign({}, config, {
+    cursorColor: 'rgba(191, 157, 183, 0.5)',
+    foregroundColor: '#737C95',
+    backgroundColor: '#30343F',
+    borderColor: '#30343F',
+    colors: {
+      black: '#30343F',
+      red: '#BF9DB7',
+      green: '#A9DDD6',
+      yellow: '#FCEDC3',
+      blue: '#91ADC2',
+      magenta: '#BF9DB7',
+      cyan: '#91ADC2',
+      white: '#FBF9F9',
+      lightBlack: '#3B404D',
+      lightRed: '#BF9DB7',
+      lightGreen: '#A9DDD6',
+      lightYellow: '#FCEDC3',
+      lightBlue: '#91ADC2',
+      lightMagenta: '#BF9DB7',
+      lightCyan: '#91ADC2',
+      lightWhite: '#FBF9F9',
+    },
+  });
+};
